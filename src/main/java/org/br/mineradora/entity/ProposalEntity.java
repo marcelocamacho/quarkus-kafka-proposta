b327bb2db90c198fc7bc91954e471552c98c468d
Marcelo S. Camacho <marcelocamacho.ufpa@gmail.com>
2024-08-02 10:41:55 -0300

    Cria a estrutura básica do projeto e a entidade Proposal

---
+++ src/main/java/org/br/mineradora/entity/ProposalEntity.java
@@ -0,0 +1,33 @@
+package org.br.mineradora.entity;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="proposal")
+@Data
+@NoArgsConstructor
+public class ProposalEntity {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String customer;
+
+    @Column(name="price_tonne")
+    private BigDecimal priceTonne;
+    private Integer tonnes;
+    private String country;
+    @Column(name="proposal_validity_days")
+    private Integer proposalValidtyDays;
+
+    @Column(name="created_at")
+    private Date createdAt;
+}
